30139ca54c44abe8e6712210795115bd7f1e8737
Viudes <pierre.antoine.viudes@gmail.com>
2023-04-28 12:03:34 +0200

    Fin des fourmis

--- calculs/distance.py
+++ calculs/distance.py
@@ -1,27 +1,29 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 import seaborn as sns
+import pylab as pl
 
 
 def distance(ville1, ville2):
     x1, y1 = ville1.x, ville1.y
     x2, y2 = ville2.x, ville2.y
 
-    d = np.sqrt((x1-x2)**2 + (y1-y2)**2)
+    d = np.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
 
     return d
 
 
 def plot_cities(villes, routes):
-
     fig, ax = plt.subplots()
 
     for ville in villes:
         x, y = ville.x, ville.y
         if ville.num == 0:
-            ax.plot(x, y , 'bo', label="nid")
+            ax.plot(x, y, 'bo', label="nid")
+            pl.text(x, y, str(ville.num))
         else:
             ax.plot(x, y, '*', label=ville.num)
+            pl.text(x, y, str(ville.num))
         ax.legend()
 
     for route in routes:
@@ -34,3 +36,15 @@ def plot_cities(villes, routes):
 def plot_pheromone(routes):
     sns.catplot([route.pheromone for route in routes], kind="bar")
     plt.show()
+
+
+def show_pheromones(routes):
+    for route in routes:
+        print("niveau de pheromone route ", route, ':', route.get_pheromone())
+
+
+def show_fourmis(fourmis):
+    for fourmi in fourmis:
+        print("ville_actuelle ", fourmi.ville_actuelle)
+        print("chemin parcouru", [str(chemin) for chemin in fourmi.chemin_parcouru])
+        print("distance parcourue depuis le nid", fourmi.distance_parcourue_depuis_nid)
--- main.py
+++ main.py
@@ -2,10 +2,12 @@
     Définition des classes de notre modèle
 """
 
-from calculs.distance import distance, plot_cities, plot_pheromone
+from calculs.distance import distance, plot_cities, plot_pheromone, show_pheromones, show_fourmis
 import numpy as np
 import random as rd
 from tqdm import tqdm
+
+from interface_graphique.interface import FenPrincipale
 
 
 class Fourmi:
@@ -15,9 +17,6 @@ class Fourmi:
         self.chemin_parcouru = []
         self.distance_parcourue_depuis_nid = 0
 
-    def step(self):
-        pass
-
 
 class Route:
 
@@ -25,10 +24,16 @@ class Route:
         self.ville_depart = ville_depart
         self.ville_arrivee = ville_arrivee
         self.longueur = distance(ville_depart, ville_arrivee)
-        self.pheromone = 0
+        self.pheromone = 1
 
     def deposer_pheromone(self):
         self.pheromone += 1
+
+    def get_pheromone(self):
+        return self.pheromone
+
+    def __str__(self):
+        return str(self.ville_depart) + '_' + str(self.ville_arrivee)
 
 
 class Ville:
@@ -38,20 +43,24 @@ class Ville:
         self.y = y
         self.num = num
 
+    def __str__(self):
+        return str(self.num)
+
 
 """
-* Création des villes en fonction des coordonnées,
-* Création des routes entre les villes avec point de départ et d'arrivée, niveau de pheromone sur les routes
-* Création des méthodes des fourmis pour avancer, step by step
-
-Création de N fourmis au point de départ
-Condition d'arrêt? ==> avoir fait N steps
-Faire un compteur pour voir où on en est sur le chemin, tant que la distance parcourue depuis ville < longeur route,
-on fait rien (la fourmi est en train de traverser la route)
-
-Quand les fourmis sont à la fin du bail, il faut les faire retourner au nid
+    * Création des villes en fonction des coordonnées,
+    * Création des routes entre les villes avec point de départ et d'arrivée, niveau de pheromone sur les routes
+    * Création des méthodes des fourmis pour avancer, step by step
+    
+    Création de N fourmis au point de départ
+    Condition d'arrêt? ==> avoir fait N steps
+    Faire un compteur pour voir où on en est sur le chemin, tant que la distance parcourue depuis ville < longeur route,
+    on fait rien (la fourmi est en train de traverser la route)
+    
+    Quand les fourmis sont à la fin du bail, il faut les faire retourner au nid
 
 """
+
 
 def step(fourmis, villes, routes):
     for fourmi in tqdm(fourmis):
@@ -63,21 +72,44 @@ def step(fourmis, villes, routes):
             possible_routes = []
             for route in routes:
                 if route.ville_depart == fourmi.ville_actuelle:
-                    possible_routes.append(route.ville_arrivee)
-            next_route = rd.choices(possible_routes)
+                    possible_routes.append(route)
+            weights = [route.get_pheromone() for route in possible_routes]
+            next_route = rd.choices(possible_routes, weights)[0]
+            fourmi.chemin_parcouru.append(next_route)
+            next_route.deposer_pheromone()
+            fourmi.ville_actuelle = next_route.ville_arrivee
+
+        elif fourmi.ville_actuelle == villes[-1]:
+            # la fourmi est arrivée au food
+            # il faut faire demi-tour
+            # print([str(chemin) for chemin in fourmi.chemin_parcouru])
+            fourmi.ville_actuelle = villes[0]    # la fourmi retourne au nid
+            for route in fourmi.chemin_parcouru:
+                route.deposer_pheromone()    # en retournant au nid, la fourmi dépose de la phéromone
+            fourmi.distance_parcourue_depuis_nid = 0
+            fourmi.chemin_parcouru = []
+            break
 
         # test si la fourmi est sur une route ou pas
-        elif fourmi.distance_parcourue_depuis_nid > fourmi.chemin_parcouru[-1]:
+        elif fourmi.distance_parcourue_depuis_nid > fourmi.chemin_parcouru[-1].longueur and len(fourmi.chemin_parcouru) > 0:
             # la fourmi a parcouru le chemin nécessaire, il faut choisir une route
             possible_routes = []
             for route in routes:
                 if route.ville_depart == fourmi.ville_actuelle:
-                    possible_routes.append(route.ville_arrivee)
-            next_route = rd.choices(possible_routes)
+                    possible_routes.append(route)
+            weights = [route.get_pheromone() for route in possible_routes]
+            next_route = rd.choices(possible_routes, weights)[0]
+            fourmi.chemin_parcouru.append(next_route)
+            next_route.deposer_pheromone()
+            fourmi.ville_actuelle = next_route.ville_arrivee
+            fourmi.distance_parcourue_depuis_nid = 0
 
         fourmi.distance_parcourue_depuis_nid += 1    # faire avancer la fourmi
 
 if __name__ == '__main__':
+
+    fen = FenPrincipale()
+    fen.mainloop()
 
     # Création des villes
     villes = []
@@ -94,14 +126,16 @@ if __name__ == '__main__':
     routes.append(Route(villes[4], villes[5]))
 
     # plot des villes
-    plot_cities(villes,routes)
+    # plot_cities(villes,routes)
 
     # création des fourmis
     fourmis = []
-    for k in range(50):
+    for k in range(15):
         fourmis.append(Fourmi(villes[0]))
 
-    for t in range(500):
+    for t in range(1000):
         step(fourmis, villes, routes)
 
-    plot_pheromone(routes)
+    show_pheromones(routes)
+    # show_fourmis(fourmis)
+    plot_cities(villes, routes)
